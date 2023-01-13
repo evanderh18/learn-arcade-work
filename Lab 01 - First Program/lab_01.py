a96b3cd7d747572211fa7c455c063a911d432ea6
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-01-12 19:05:43 -0700

    Added some pretty neat print statements

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,10 @@
 print("Hello World")
-print('Hello World!')
+print('What an amazing world')
+print("This is what a double quote marks looks: \" like in python.")
+print("Evan once said \"I am a pretty cool guy!\"")
+print("This\nis\nmy\nmy\nname")
+print("This \n is \n my \n name")
+print("""This is
+how you print
+multiple lines
+on python""")
