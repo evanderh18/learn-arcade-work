9953029eafbf3b9b46e7fe8285220ac49bd1baed
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-01-17 18:09:38 -0700

    Added a tab, made format changes

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,5 +1,5 @@
 print("Hello World")
-print('What an amazing world')
+print('What an "amazing" world')
 print("This is what a double quote mark looks like: \"  in python.")
 print("Evan once said \"I am a pretty cool guy!\"")
 print("This\nis\nmy\nname")
@@ -8,3 +8,4 @@ print("""This is
 how you print
 multiple lines
 in python""")
+print("This is what a tab looks like in python \tpretty cool eh?")
