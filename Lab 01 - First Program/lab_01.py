5006b641db591298af250b6d7d2be5c950461a88
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-01-17 17:45:36 -0700

    Added grammer fixes

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,8 +1,8 @@
 print("Hello World")
 print('What an amazing world')
-print("This is what a double quote marks looks: \" like in python.")
+print("This is what a double quote mark looks: \" like in python.")
 print("Evan once said \"I am a pretty cool guy!\"")
-print("This\nis\nmy\nmy\nname")
+print("This\nis\nmy\nname")
 print("This \n is \n my \n name")
 print("""This is
 how you print
