beccc42164dd276c7aaeaaf60f6d87cb9656c38a
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-01-17 17:58:56 -0700

    Made important format changes

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -3,7 +3,7 @@ print('What an amazing world')
 print("This is what a double quote mark looks like: \"  in python.")
 print("Evan once said \"I am a pretty cool guy!\"")
 print("This\nis\nmy\nname")
-print("My\nname\nis\nEvan")
+print("My name\nis Evan")
 print("""This is
 how you print
 multiple lines
