ba315ebba07ee423b82ae32852179dc980ddea41
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-01-17 17:51:55 -0700

    Added my name, and formatted my code

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,10 +1,10 @@
 print("Hello World")
 print('What an amazing world')
-print("This is what a double quote mark looks: \" like in python.")
+print("This is what a double quote mark looks like: \"  in python.")
 print("Evan once said \"I am a pretty cool guy!\"")
 print("This\nis\nmy\nname")
-print("This \n is \n my \n name")
+print("My\nname\nis\nEvan")
 print("""This is
 how you print
 multiple lines
-on python""")
+in python""")
