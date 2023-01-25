0c4b0c09a9ce4a89163293d8a55a51618ab78340
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-01-24 18:38:12 -0700

    Made some progress on snow person

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -1,20 +1,36 @@
+# Import "Arcade" library
+
 import arcade
 
+# Create a window
 arcade.open_window(600,600, "snow_person")
-#background
+
+# Background
 arcade.set_background_color(arcade.csscolor.LIGHT_BLUE)
 
 arcade.start_render()
-#ground
-arcade.draw_lrtb_rectangle_filled(0, 600, 300, 0, arcade.csscolor.WHITE_SMOKE)
+# Ground
+arcade.draw_lrtb_rectangle_filled(0, 600, 300, 0, arcade.csscolor.ALICE_BLUE)
 
-#body
-arcade.draw_circle_filled(300, 175, 135, arcade.csscolor.WHITE)
+# Body
+arcade.draw_circle_filled(300, 175, 135, arcade.csscolor.WHITE_SMOKE)
+arcade.draw_circle_filled(300, 330, 95, arcade.csscolor.MINT_CREAM)
+arcade.draw_circle_filled(300, 460, 70, arcade.csscolor.WHITE)
 
-#arms
+# Arms
 
-arcade.draw_rectangle_filled(180, 320, 10, 185, arcade.csscolor.SADDLE_BROWN, 50)
-arcade.draw_rectangle_filled(420, 320, 10, 185, arcade.csscolor.SADDLE_BROWN, 130)
+arcade.draw_rectangle_filled(150, 310, 10, 185, arcade.csscolor.SADDLE_BROWN, 50)
+arcade.draw_rectangle_filled(450, 310, 10, 185, arcade.csscolor.SADDLE_BROWN, 130)
 
+# Hands
+
+# Left Hand
+arcade.draw_rectangle_filled(113, 253, 10, 50, arcade.csscolor.SADDLE_BROWN, 180)
+arcade.draw_rectangle_filled(100, 285, 10, 37, arcade.csscolor.SADDLE_BROWN, 100)
+# Right Hand
+arcade.draw_rectangle_filled(493, 250, 10, 44, arcade.csscolor.SADDLE_BROWN, 190)
+arcade.draw_rectangle_filled(510, 285, 10, 55, arcade.csscolor.SADDLE_BROWN, 85)
+
+# Botton
 
 arcade.run()
