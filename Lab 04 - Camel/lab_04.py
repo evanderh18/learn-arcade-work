2b34f50b0401b9cc1e2c5db1d07d446667f0b7f8
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-02-07 19:03:22 -0700

    Started lab 4

---
+++ Lab 04 - Camel/lab_04.py
@@ -0,0 +1,21 @@
+# Welcome Screen
+def main():
+    print("Welcome to Camel!")
+    print("You have stolen a camel to make your way across the great Mobi desert.")
+    print("The natives want their camel back and are chasing you down! Survive your")
+    print("desert trek and out run the natives.")
+
+
+done = False
+while done == False:
+    print("A. Drink from your canteen.")
+    print("B. Ahead moderate speed.")
+    print("C. Ahead full speed.")
+    print("D. Stop for the night.")
+    print("E. Status check.")
+    print("E. Status check.")
+
+
+
+
+main()
