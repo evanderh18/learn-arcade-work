6133d542076f1d9845b88a4ead691df8717b8a1a
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2023-04-15 17:14:41 -0600

    Added some things for part 3

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -1,5 +1,8 @@
 import random
 import math
+ROOM_MIN = 1
+ROOM_MAX = 7
+
 
 class Room:
     def __init__(self, description: str = "", north: int = 0, east: int = 0, south: int = 0, west: int = 0):
@@ -10,18 +13,31 @@ class Room:
         self.west: int = west
 
 class Item:
-    def __init__(self, description: str = "", room_num: int = "", name: str = ""):
+    def __init__(self, description: str = "", room_num: int = 0, name: str = ""):
         self.description: str = description
         self.room_num: int = room_num
         self.name: str = name
 
+class Ghost:
+    def __init__(self, room_num: int = 1):
+        self.room_num: int = room_num
+
 def main():
     room_list = []
     item_list = []
-    item_list.append(Item("There is a invisibility potion here", random.randint(0, 7), "Potion"))
-    item_list.append(Item("A Key that gives you access to another room", random.randint(0, 7), "Key"))
-    item_list.append(Item("You found sage", random.randint(0, 7), "Sage"))
-    item_list.append(Item("You found a match", random.randint(0, 7), "Match"))
+    ghost_list = []
+
+    lives = 3
+
+    input("---Welcome to the Haunted Mansion---\nYour objective is to find your way out hahaha!\nFind a key to open "
+          "up the door to the main exit\n---PRESS ANY KEY TO CONTINUE---")
+
+    ghost_list.append(Ghost(random.randint(ROOM_MIN, ROOM_MAX)))
+
+    item_list.append(Item("There is a invisibility potion here", random.randint(ROOM_MIN, ROOM_MAX), "Potion"))
+    item_list.append(Item("A Key that gives you access to another room", random.randint(ROOM_MIN, ROOM_MAX), "Key"))
+    item_list.append(Item("You found sage", random.randint(ROOM_MIN, ROOM_MAX), "Sage"))
+    item_list.append(Item("You found a match", random.randint(ROOM_MIN, ROOM_MAX), "Match"))
 
     room = Room("You are in a bedroom that has two small beds.\nThere is a door to your North and East.", 3, 1, None,
                 None)
@@ -49,10 +65,17 @@ def main():
 
     done = False
     while not done:
+        caught_by_ghost = False
         print("\n" + room_list[current_room].description)
         for f in item_list:
             if f.room_num == current_room:
                 print(f.description)
+        for f in ghost_list:
+            if f.room_num == current_room:
+                print("Zoinks! A ghost has shown itself!\nSave yourself with sage and a match if you have one\nUse"
+                      "command\"USE Sage\"")
+                caught_by_ghost = True
+                break
         user_input = input("What is your action? ")
         command_words = user_input.split(" ")
         if command_words[0].upper() == "N" or command_words[0].upper() == "NORTH":
@@ -79,7 +102,7 @@ def main():
         elif command_words[0].upper() == "USE":
             for f in item_list:
                 if f.name.upper() == command_words[1].upper():
-                    f.room_num = random.randint(0, 7)
+                    f.room_num = random.randint(ROOM_MIN, ROOM_MAX)
         elif command_words[0].upper() == "Q" or command_words[0].upper() == "QUIT":
             done = True
             print("\nYou left the house\nGame Over")
@@ -92,7 +115,13 @@ def main():
         if next_room is None:
             print("There is no room here!")
         else:
+            if caught_by_ghost == True:
+                print("The ghost took one of your lives")
+                lives -= 1
+                print(lives)
             current_room = next_room
+            for f in ghost_list:
+                f.room_num = random.randint(ROOM_MIN, ROOM_MAX)
 
 
 main()
